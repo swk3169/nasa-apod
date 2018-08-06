8d43aa9da1b93acce7f62dfdc91b98683cb3d17c
swk3169 <swk3169@gmail.com>
2018-08-07 04:12:34 +0900

    ver0.04
    +Api Complete

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export function getAPOD(date = '') {
-    return axios.get(`https://api.nasa.gov/planetary/apod?api_key=5pFVJLpTTrq0q8NggLnQSYmRB7KU1GCcepChVRu8&date=${date}`);
+    return axios.get(`https://api.nasa.gov/planetary/apod?api_key=&date=${date}`);
 }
