809864f8fbcc6bed23a6bf6de63618870c85c6f0
swk3169 <swk3169@gmail.com>
2018-08-07 03:12:42 +0900

    ver0.03
    +Picture Complete
    +Video Complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import ViewerTemplate from './components/ViewerTemplate';
 import SpaceNavigator from './components/SpaceNavigator';
+import Viewer from './components/Viewer';
 
 class App extends Component {
   render() {
     return (
       <ViewerTemplate
         spaceNavigator={<SpaceNavigator/>}
+        viewer={(
+          <Viewer
+            url="https://www.youtube.com/embed/uj3Lq7Gu94Y?rel=0"
+            mediaType="video"/>
+        )}
       />
     );
   }
--- src/components/Viewer/Viewer.js
+++ src/components/Viewer/Viewer.js
@@ -5,9 +5,17 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 const Viewer = ({mediaType, url, loading}) => {
+    if(!url) return null;
+
     return (
         <div className={cx('viewer')}>
-
+            {
+                mediaType === 'image' ? (
+                    <img onClick={() => window.open(url)} src={url} alt="space"/>
+                ) : (
+                    <iframe title="space-video" src={url} frameBorder="0" gesture="media" allow="encrypted-media" allowFullScreen></iframe>
+                )
+            }
         </div>
     );
 };
