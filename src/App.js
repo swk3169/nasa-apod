18c632164055aab262da093c9b60ae8bd21cd4ea
swk3169 <swk3169@gmail.com>
2018-08-07 01:50:10 +0900

    ver0.02
    +SpaceNavigator Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import ViewerTemplate from './components/ViewerTemplate';
+import SpaceNavigator from './components/SpaceNavigator';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <ViewerTemplate
+        spaceNavigator={<SpaceNavigator/>}
+      />
     );
   }
 }
